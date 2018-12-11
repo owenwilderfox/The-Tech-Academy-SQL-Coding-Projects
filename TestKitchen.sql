f9d5df813fdc65940be550fed279f43e19696d08
Savannah Fox <savannahfox@gmail.com>
2018-12-11 22:44:22 +0000

    Delete TestKitchen.sql
    Clean up repository, remove unnecessary file.

--- TestKitchen.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM books;
-SELECT * FROM book_authors;
-
-SELECT Title, AuthorName
-FROM books
-INNER JOIN book_authors ON book_authors.BookID = books.BookID;
-
-
-SELECT * FROM library_branch;
