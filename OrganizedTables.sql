cb4ae407b7755c9d11efaaa886ab38f40e5a4059
Savannah Fox <savannahfox@gmail.com>
2018-12-11 11:07:25 -0800

    Update OrganizedTables.sql

--- OrganizedTables.sql
+++ OrganizedTables.sql
@@ -6,3 +6,8 @@ FROM books
 INNER JOIN publisher ON publisher.PublisherName = books.PublisherName
 INNER JOIN book_authors ON book_authors.BookID = books.BookID
 ;
+
+SELECT library_branch.BranchID AS 'Branch ID', library_branch.BranchName AS 'Branch Name', library_branch.Address AS 'Branch Address', book_loans.BookID AS 'Book ID', book_loans.CardNo AS 'Library Card Number', book_loans.DateOut AS 'Borrow Date', book_loans.DateDue AS 'Return Date'
+FROM library_branch
+INNER JOIN book_loans ON book_loans.BranchID = library_branch.BranchID
+;
